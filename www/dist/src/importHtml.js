f5c31238cc82813cb7ac7d6eec02702324844995
jeonga lee <lja3248@gmail.com>
2021-12-14 16:49:48 +0900

    [feat] javascript : headBox & footBox make temp and feath

---
+++ www/dist/src/importHtml.js
@@ -0,0 +1,20 @@
+
+// data 변수
+const headerCode = '../../temp/headBox.html';
+const footerCode = '../../temp/footBox.html';
+
+
+// 선택 변수
+const headBox = document.querySelector('#headBox');
+const foorBox = document.querySelector('#footBox');
+
+
+//함수
+const fnFetch = (dataCode, selector)=>{
+  fetch(dataCode)
+  .then( response => response.text() )
+  .then( data => selector.innerHTML = data );
+};
+
+fnFetch(headerCode, headBox);
+fnFetch(footerCode, foorBox);
