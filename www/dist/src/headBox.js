98ae585561aa3583de3e528fb42d10da16cecb58
jeonga lee <lja3248@gmail.com>
2021-12-16 23:47:27 +0900

    [fix] image link  modify

--- www/dist/src/headBox.js
+++ www/dist/src/headBox.js
@@ -1,14 +1,19 @@
 //headBox.js
+// (()=>{
+// })();//즉시실행함수 끝!
+  const elHeaderWrap = document.querySelector('.headerWrap');
+  const elGnbBox = elHeaderWrap.querySelector('#gnbBox');
+  const elGnbInner = elGnbBox.querySelector('.gnb_inner');
+  const elGnbInnerLi = elGnbInner.children;
+  const elGnbInnerList = [].slice.call(elGnbInnerLi);
 
-const elHeaderWrap = document.querySelector('.headerWrap');
-const elGnbBox = elHeaderWrap.querySelector('#gnbBox');
-
-const elGnbOpenBtn = elHeaderWrap.querySelector('.gnb_btn');
-const elGnbOpenButton = elGnbOpenBtn.querySelector('button');
-
-const elGnbClosebtn = elHeaderWrap.querySelector('#gnb_close');
-const elGnbCloseButton = elGnbClosebtn.querySelector('button');
-
+  
+  const elGnbOpenBtn = elHeaderWrap.querySelector('.gnb_btn');
+  const elGnbOpenButton = elGnbOpenBtn.querySelector('button');
+  
+  const elGnbClosebtn = elHeaderWrap.querySelector('#gnb_close');
+  const elGnbCloseButton = elGnbClosebtn.querySelector('button');
+  
 
 const elSearchWrap = document.querySelector('.search_wrapper');
 const elSearchBtn = elSearchWrap.querySelector('.search_btn');
@@ -19,6 +24,10 @@ const elSearchCloseBtn = elSearchWrap.querySelector('#search_close');
 const elSearchCloseButton = elSearchCloseBtn.querySelector('button');
 
 
+const ACTION = 'action';
+
+
+// open, close 버튼
 
 elGnbOpenButton.addEventListener('click', (e)=>{
   e.preventDefault();
@@ -40,3 +49,29 @@ elSearchCloseButton.addEventListener('click', (e)=>{
   e.preventDefault();
   elSearchBox.style = 'display:none';
 })
+
+const silbling = (selector, idx)=>{
+  const _array = [];
+  selector.forEach( (data, i)=>{
+    if(idx !== i){ _array.push(data); }
+  })
+  return _array;
+}
+
+let IDX = 0;
+elGnbInnerList.forEach( (data, index)=>{
+  const _gnbInnerTitle = data.querySelector('.gnb_inner_title > a');
+  const _gnbInnerContent = data.querySelector('.gnb_inner_con');
+
+  _gnbInnerTitle.addEventListener('click', (e)=>{
+    e.preventDefault();
+    // ( IDX !== index ) ? _gnbInnerContent.classList.remove(ACTION) : _gnbInnerContent.classList.add(ACTION);
+
+      _gnbInnerContent.classList.remove(ACTION);
+      IDX = index;
+      _gnbInnerContent.classList.add(ACTION);
+
+  })
+});
+
+
